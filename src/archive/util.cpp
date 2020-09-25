568ad87bba1d9adbcc4e99f9c130f1d0cd1cca73
Ingo Ruhnke <grumbel@gmail.com>
2020-09-25 17:10:40 +0200

    Fix std::ifstream exception handling
    std::ifstream::failbit is set when read() is trying to read more data
    than is available, which is normal.

--- src/archive/util.cpp
+++ src/archive/util.cpp
@@ -18,6 +18,7 @@
 
 #include "archive/util.hpp"
 
+#include <errno.h>
 #include <fstream>
 #include <stdexcept>
 
@@ -26,24 +27,30 @@
 
 void write_file(std::filesystem::path const& filename, std::span<uint8_t const> data)
 {
-  std::ofstream out;
-  out.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-  out.open(filename, std::ifstream::binary);
+  std::ofstream out(filename, std::ifstream::binary);
+  if (!out) {
+    throw std::system_error(errno, std::system_category(), "failed to open " + filename.string());
+  }
 
-  out.write(reinterpret_cast<char const*>(data.data()), data.size());
+  if (!out.write(reinterpret_cast<char const*>(data.data()), data.size())) {
+    throw std::system_error(errno, std::system_category(), "failed to write " + filename.string());
+  }
 }
 
 std::vector<uint8_t> read_file(std::filesystem::path const& filename)
 {
-  std::ifstream in;
-  in.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-  in.open(filename, std::ifstream::binary);
+  std::ifstream in(filename, std::ifstream::binary);
+  if (!in) {
+    throw std::system_error(errno, std::system_category(), "failed to open " + filename.string());
+  }
 
   std::vector<uint8_t> data;
-
   uint8_t buffer[4096];
   while(!in.eof()) {
-    in.read(reinterpret_cast<char*>(buffer), 4096);
+    if (!in.read(reinterpret_cast<char*>(buffer), 4096) && !in.eof()) {
+      throw std::system_error(errno, std::system_category(), "failed to read " + filename.string());
+    }
+    data.reserve(data.size() + in.gcount());
     std::copy_n(buffer, in.gcount(), std::back_inserter(data));
   }
 
