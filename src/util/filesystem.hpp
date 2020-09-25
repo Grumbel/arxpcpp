2fa4a18205a414f925276b32a1d3cad59318f9fc
Ingo Ruhnke <grumbel@gmail.com>
2020-09-25 19:19:31 +0200

    Remove unused/obsolete code from Filesystem

--- src/util/filesystem.hpp
+++ src/util/filesystem.hpp
@@ -17,43 +17,18 @@
 #ifndef HEADER_GALAPIX_UTIL_FILESYSTEM_HPP
 #define HEADER_GALAPIX_UTIL_FILESYSTEM_HPP
 
+#include <filesystem>
+
 class Filesystem
 {
-private:
-  static std::string home_directory;
-
 public:
-  static std::string find_exe(const std::string& name);
-
-  static bool is_directory(const std::string& pathname);
-  static bool exist(const std::string& pathname);
-  static void mkdir(const std::string& pathname);
-  static void remove(const std::string& filename);
-  static std::vector<std::string> open_directory(const std::string& pathname);
-
-  static void open_directory_recursivly(const std::string& pathname, std::vector<std::string>& lst);
-
-  static void readlines_from_file(const std::string& pathname, std::vector<std::string>& lst);
-
-  static std::string getxattr(const std::string& pathname);
-  static std::string get_home() { return home_directory; }
-
-  static std::string realpath_system(const std::string& pathname);
-  static std::string realpath_fast(const std::string& pathname);
-  static std::string realpath(const std::string& pathname);
+  static std::filesystem::path find_exe(const std::string& name);
 
   static std::string get_extension(const std::string& pathname);
   static bool has_extension(const std::string& filename, const std::string& ext);
-  static void copy_mtime(const std::string& from_filename, const std::string& to_filename);
 
   /** read the first 512 bytes of the archive for magic detection */
   static std::string  get_magic(const std::string& filename);
-
-  static time_t get_mtime(const std::string& filename);
-  static size_t get_size(const std::string& filename);
-
-  static void init();
-  static void deinit();
 };
 
 #endif
