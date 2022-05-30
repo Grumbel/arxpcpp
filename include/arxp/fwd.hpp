81ba68dbb6d19b77123f0332e175d0d277229e01
Ingo Ruhnke <grumbel@gmail.com>
2022-05-30 13:33:14 +0200

    Add forward declarations header

---
+++ include/arxp/fwd.hpp
@@ -0,0 +1,26 @@
+#ifndef HEADER_ARPX_FWD_HPP
+#define HEADER_ARPX_FWD_HPP
+
+namespace arxp {
+
+class ArchiveHandle;
+class ArchiveLoader;
+class ArchiveManager;
+class DirectoryExtraction;
+class Extraction;
+class IncrementalExtraction;
+class Rar;
+class RarArchiveLoader;
+class RarHeader;
+class SevenZip;
+class SevenZipArchiveLoader;
+class Tar;
+class TarArchiveLoader;
+class Zip;
+class ZipArchiveLoader;
+
+} // namespace arxp
+
+#endif
+
+/* EOF */
